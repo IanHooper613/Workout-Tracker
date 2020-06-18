e5f2903efb19310c483ec414b24e035749836bb5
Ian Hooper <ianhooper8@gmail.com>
2020-06-17 23:58:08 -0400

    app running on port 3000

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = requre('express')
+const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('morgan')
 
@@ -13,7 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+require('./routes/htmlRoutes.js')(app)
+require('./routes/apiRoutes.js')(app)
+
 
 
 app.listen(PORT, () => {
