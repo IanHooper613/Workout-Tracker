760ba7faae92da2023ad2fd398715bc8ec6c7db4
Ian Hooper <ianhooper8@gmail.com>
2020-06-18 15:30:14 -0400

    adding in all api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,31 @@ module.exports = function(app) {
             res.status(400).json(error)
         })
     })
+
+    app.post('/api/workouts', ({ body }, res) => {
+        Workout.create(body)
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+    })
+
+    app.put('/api/workouts/:id', (req, res) => {
+        Workout.updateOne(
+            { _id: req.params.id },
+            { 
+                $push: { exercises: req.body },
+                $inc: { totalDuration: req.body.duration }
+            }    
+    )
+    .then(newWorkout => {
+        res.json(newWorkout);
+    })
+    .catch(error => {
+        res.status(400).json(error)
+    });
+})
 }
