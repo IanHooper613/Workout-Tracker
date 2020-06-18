e8bafaaa541bf0bc20c171229e192de40da34536
Ian Hooper <ianhooper8@gmail.com>
2020-06-18 16:33:59 -0400

    adding index.html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,5 +8,10 @@ module.exports = function(app) {
     app.get('/stats', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/stats.html'));
     });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
 }
 
