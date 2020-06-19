ba7e773e0cedc3c745e109651b725b0eaffd13fe
Ian Hooper <ianhooper8@gmail.com>
2020-06-18 21:37:47 -0400

    adding notes to server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+//require npm packages express, mongoose, and morgan
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('morgan')
 
+//creating Port
 const PORT = process.env.PORT || 3000;
 
+//setting up express app
 const app = express();
+
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
 
+//setting up middleware for express
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,11 +22,12 @@ app.use(express.static('public'));
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
+//routes
 require('./routes/htmlRoutes.js')(app)
 require('./routes/apiRoutes.js')(app)
 
 
-
+//listen on Port
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
 })
