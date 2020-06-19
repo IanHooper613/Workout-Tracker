e03e4add62133a5268f34cd7b0d4b151d7f5467b
Ian Hooper <ianhooper8@gmail.com>
2020-06-18 21:36:59 -0400

    adding notes to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,16 @@
+//require path
 const path = require('path')
 
 module.exports = function(app) {
+    //exercise page
     app.get('/exercise', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/exercise.html'));
     });
-
+    //stats page
     app.get('/stats', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/stats.html'));
     });
-
+    //root route
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
